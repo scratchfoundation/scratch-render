672947ca56cb6b88ace0e58c11fae43dc642dd5b
Andrew Sliwinski <andrewsliwinski@acm.org>
2016-08-08 14:22:54 -0400

    Normalize build result naming to match VM conventions

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,13 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: {
-        'render-webgl': './src/index-web.js',
-        'render-webgl.min': './src/index-web.js',
-        'render-webgl-worker': './src/index-webworker.js',
-        'render-webgl-worker.min': './src/index-webworker.js'
+        'render': './src/index-web.js',
+        'render.min': './src/index-web.js',
+        'render-worker': './src/index-webworker.js',
+        'render-worker.min': './src/index-webworker.js'
     },
-    devtool: 'source-map',
     output: {
-        path: path.join(__dirname, 'build'),
+        path: __dirname,
         filename: '[name].js'
     },
     module: {
@@ -31,7 +30,7 @@ module.exports = {
             },
             {
                 test: /\.(glsl|vs|fs|frag|vert)$/,
-                loader: 'raw-loader' // we might want a GLSL loader if we use includes
+                loader: 'raw-loader'
             }
         ]
     },
