ee7c1860e3601d4f4e0ab492c59d93a85caaad56
GitHub <noreply@github.com>
2017-01-25 14:56:19 +0000

    Build for 9841de85f1faf620535e49771af34f9ca19cc617 2fc9005c6012a07eb1f6f10718e3dd85ebd617de bf2863921c1e48bc41198d8a5f3eabf205644175 c478ebec359b338a38053ff9ff72d133235b652c ea786aa4bd167542f87d5f4ce3f6e6fec76b282a abd76720eea77ce364a3706adecabec9609c4c9d 5eef82a9dc71623ddb9c3495e4b9949b68e964a0 3bdb1fff1f7d274711790ac015dd2b3a1faa286d ce5a3b8ad64c917de191500b5e470c825cd77509 f393d86b8ee386b155866031c4049ea2f4c36e67 a4b863e46438f37e75657b43ec1d73583302e0b9 875a5a3ec17b72d08bf589f52b7f3ea47464774c 51e8aa9b1a43335afac703534d3536d77add15cc 1531ea8ae6075771ee3f578b002819d98246b4f0 f7fd05cfba647af542ef6b25ceddc5737a278e78 f5af954acbda6d01ab0c50ca9752d457f28de160 c2ca8d2a8403efcb051c12ff572cb787ddb1b012 78c0d09c72a61420112ceb2b2b3a51e943da000c f29ed34ddc9c556c95117b0b6aa9459ffc6457db 140c0fbf3785d1c2531a2d7ac19f86d017236603 1f0cd4b61f58e6b7b2c13868a4be29453c7acdf0 6643469ff267be96612ebf9630d84d2736f0b4f3 6a0798062bf67424be59d093a99050abe0df2f4d ba5deb9936320b33879d6f98df339cb087eca51a 64c59914861a47162aee48a8e9f60854118617e8 4730a057ffb56e7f2a7a24d01daaa1e6789b8be5 e3a05743b0b9507d985fa27cd2e725b0e5afac45 2a64c1dd1811dedd96915460055a8c225c588d3d c6356f71ff20ba5c520c5ff845c4208d4af217a7 2dafa70ff7bd479f1d24333226d68ecaf5d2ca87 71ad4a8a8083439fecc2c45b2a530fae57eef46d 8164fef77e33563e761e138f7f02f749f5523781 14b34f4b3c6e2f7a153e5cd90af50af7a6ac396e b18ddadbe662edf559f23e2b31edf216fea28a2b 4afbba3657fabca1205bc12aa67f8269fda69d11 d1ea82b2bfab108b4f2e5abd132fb104df4dce86 b70056019827f72f5d6451163a25dc8fb9e2f1f4 b950b9f9adf2a705678bae2bb557c2dffe6f461d 3c80ed73a7c46f71896da6a9ecf3868cddf41eb4 5db860efedcc56bdf1f2c58ac74de1e1a7d76af4 47b46fb1aaa7f0d1cddb50cd309030bfd2f0efd3 b35f684478931d46925e9c11c3b264360e0c352f 8be289b7eec87561e263a3bfcd5fa1809ae173dc 722d7224f900f56d5fe20e82d4cca23946889792 85bdbdb721153eaf04f7cbf2ed2632ae6c02cdaa 6c4715f7935b42316b958e4f9025958d15654d05 be6d2dc4e4d739ef6dd354578105d7cf20e5fab2 2cffa7b643abf909daa3db10ba5ccf4a326ff91b a484fda1a6925b1e86a1bef470964f5b64c71a26 662ba3c645fa696e95f07b45e39e9c039bf24bc3 85c8b599a029607d5303695e895170fe520842e4 8e7a85d1a6addf8db6adeefc341e5470b7e1b8fd c00f0f55fc3daecac9fddfda99923e2d5d48c5bf 1fa97d248b2cfcacac041fd8eb97c99101ef4c5f a62d52a2d0655be3680c637fdaa05f2061e724fc f1ac1a8211de8fc551846a60e3c38b7262031bd5 14018f5ff66f8b401944fc4741ecaa93ce4e0b02 7629b2ab6d3b854ba5c85390e5eca76cdc3bf68d d261b57cd182fb654b515fbbf86103fae6efb5c5 d813dd91913fe2d8f927411e09d3f7a8ae60bbb3 053092c85b769a4686a3a78776e984324d0801be 9fa35524de0fa0261f62cc28608a770e2ef9b1e3 d7d1f92a131e94c8d0f7df717cedbb0a1c263cb2 59337ef8b5186487204855ebf49d0ef849770076 09dc67fa83ad34dd454f475610e8552f3b4b79c6 856f919970cbd74387c32bbd9f3945a68721b0f5 9f26ab17eb3ef3ca5a311ad1777ab506439338a6 d91ddf415851460095eb063059810d3441a1b469 201ceaba7752d75fc2cd9c4425b4920d2fbc0ad8 a6c8be822603fa120a4abc945fdd7edd64863aa8 89402a88c7e1953ee6bb7f70ef1dd39cb84896d6 278cddf04e9bf7f9da9a761be103f17bb4d824ac fb3f677e2c39cb04e7e49302eefef9b76a7b270d a5fb65ebebf614840017298e6d834c5c5e10af9b 2b81b791b325b3d02f12754ca99e3727d0705a0d 042f0f035304fd342d4657134844a126a2c74409 69821db56f549a311d4ea633354e20bcccf962d4 3277584da98b012d545ab9bfaa224ff74d64ec98 19bb8b99c33c87266aacfb9997a788dbb28e859a bf26fe41e8effeccd29be8ac6af04addc78b52f8 92fc916b866854767ac0f43ba56447c454b92af1 f5cd625be8baa78d0665abbbabeda7367407cc2f b8cc5f5245160eb02e1c2e2eb4854434f6956b96 47b5d07497cc59eeaa3affdf3013e759951f958e 672947ca56cb6b88ace0e58c11fae43dc642dd5b fc7db059784667af806c3cc0ba01044b95406d50 3dca142bb931c9dcd76930efdaaa9c9a1dcbc7f9 894ada4960514199cdbce1f04fcb53f5ab6f1ff5 86ae0d63e3fe4fad55d737b882c8725580c96d95 5c75fc011265679cbfa5f8af6f12edaa3236a974 543f05d6d41221c011af4ff962087e29669a4053 ef100bf91ee1f0f0a451c6fad49f9204ecd365e0 b00ec4c50100223c043556363f990e8fd9d27848 ad94df4afe7eea6e2cfadb5503065aa643a78e6d 2c9e17d486da7585dda6548885b5b00b185ef356 f3a80c3429a11fb9d22f24d1f08f7efc4d414d9c 73ae8c205d7b817c1bc56b5516204e69c7418b6d

--- scratch-render.js
+++ scratch-render.js
@@ -20017,6 +20017,7 @@
 	        value: function clear() {
 	            var ctx = this._canvas.getContext('2d');
 	            ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+	            this._canvasDirty = true;
 	        }
 	
 	        /**
