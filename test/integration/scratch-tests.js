27462ef61e8a7d799663dd5e4d367a3670665cd3
Corey Frang <gnarf37@gmail.com>
2018-04-25 11:00:26 -0400

    add some more comments for the integration helper and add a timeout

--- test/integration/scratch-tests.js
+++ test/integration/scratch-tests.js
@@ -13,17 +13,29 @@ const testFile = file => {
             .setFileInput('#file', fullPath)
             .wait('#loaded')
             .evaluate(() => {
+                // This function is run INSIDE the integration chrome browser via some
+                // injection and .toString() magic.  We can return some "simple data"
+                // back across as a promise, so we will just log all the says that happen
+                // for parsing after.
                 const says = [];
+                const TIMEOUT = 5000;
 
                 vm.runtime.on('SAY', (_, __, message) => {
                     says.push(message);
                 });
 
                 vm.greenFlag();
+                const startTime = Date.now();
 
                 return Promise.resolve()
                     .then(async () => {
+                        // waiting for all threads to complete, then we return
                         while (vm.runtime.threads.length > 0) {
+                            if ((Date.now() - startTime) >= TIMEOUT) {
+                                says.push(`fail Threads still running after ${TIMEOUT}ms`);
+                                break;
+                            }
+
                             await new Promise(resolve => setTimeout(resolve, 50));
                         }
 
