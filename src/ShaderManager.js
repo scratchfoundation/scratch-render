c08b8535197bbefc2d39f064864e09b0fbde6d22
Michael "Z" Goddard <mzgoddard@gmail.com>
2018-08-08 14:04:37 -0400

    add uniformName to ShaderManager EFFECT_INFO entries (#328)

--- src/ShaderManager.js
+++ src/ShaderManager.js
@@ -88,30 +88,35 @@ class ShaderManager {
 ShaderManager.EFFECT_INFO = {
     /** Color effect */
     color: {
+        uniformName: 'u_color',
         mask: 1 << 0,
         converter: x => (x / 200) % 1,
         shapeChanges: false
     },
     /** Fisheye effect */
     fisheye: {
+        uniformName: 'u_fisheye',
         mask: 1 << 1,
         converter: x => Math.max(0, (x + 100) / 100),
         shapeChanges: true
     },
     /** Whirl effect */
     whirl: {
+        uniformName: 'u_whirl',
         mask: 1 << 2,
         converter: x => -x * Math.PI / 180,
         shapeChanges: true
     },
     /** Pixelate effect */
     pixelate: {
+        uniformName: 'u_pixelate',
         mask: 1 << 3,
         converter: x => Math.abs(x) / 10,
         shapeChanges: true
     },
     /** Mosaic effect */
     mosaic: {
+        uniformName: 'u_mosaic',
         mask: 1 << 4,
         converter: x => {
             x = Math.round((Math.abs(x) + 10) / 10);
@@ -122,12 +127,14 @@ ShaderManager.EFFECT_INFO = {
     },
     /** Brightness effect */
     brightness: {
+        uniformName: 'u_brightness',
         mask: 1 << 5,
         converter: x => Math.max(-100, Math.min(x, 100)) / 100,
         shapeChanges: false
     },
     /** Ghost effect */
     ghost: {
+        uniformName: 'u_ghost',
         mask: 1 << 6,
         converter: x => 1 - (Math.max(0, Math.min(x, 100)) / 100),
         shapeChanges: false
