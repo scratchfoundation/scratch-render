6cadd0b31a955339fa3382371c1d9fedeef39e1a
Christopher Willis-Ford <cwillisf@media.mit.edu>
2019-03-20 22:58:32 -0700

    test: make touching-color test more robust against GPU imprecision
    Previously, the `color-touching-tests.sb2` test "touches a color that
    doesn't actually exist right now" would use a sprite with ghost 50,
    blended against another sprite, to create the color that "doesn't
    actually exist" when the query sprite is skipped. Unfortunately the
    blend result was near a bit-boundary and, depending on the specific
    hardware used, that test could fail on the GPU. When the renderer uses
    the CPU path this test works fine, though, so the existing problem went
    unnoticed.
    
    To fix the problem I changed the project to use ghost 30 instead, which
    results in a color that is less near a bit boundary and is therefore
    less likely to fail on specific hardware.
    
    As an example of what was happening: the `touching color` block was
    checking for `RGB(127,101,216)` with a mask of `RGB(0xF8,0xF8,0xF0)`. On
    the CPU it would find `RGB(120,99,215)`, which is in range, but on some
    GPUs the closest color it could find was `RGB(119,98,215)` which
    mismatches on all four of the least significant bits -- one of which is
    enabled in the mask.

--- src/RenderWebGL.js
+++ src/RenderWebGL.js
@@ -831,7 +831,7 @@ class RenderWebGL extends EventEmitter {
                         debugCanvasContext.fillRect(x - bounds.left, bounds.bottom - y, 1, 1);
                     }
                     // ...and the target color is drawn at this pixel
-                    if (colorMatches(color, color3b, 0)) {
+                    if (colorMatches(color3b, color, 0)) {
                         return true;
                     }
                 }
