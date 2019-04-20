750f40ddf228c1404fdb6b94a3dd344f48b8cd43
adroitwhiz <adroitwhiz@protonmail.com>
2019-04-20 02:31:04 -0400

    Exit draw region in penStamp less

--- src/RenderWebGL.js
+++ src/RenderWebGL.js
@@ -1409,8 +1409,6 @@ class RenderWebGL extends EventEmitter {
      * @param {int} stampID - the unique ID of the Drawable to use as the stamp.
      */
     penStamp (penSkinID, stampID) {
-        this._doExitDrawRegion();
-
         const stampDrawable = this._allDrawables[stampID];
         if (!stampDrawable) {
             return;
@@ -1420,6 +1418,8 @@ class RenderWebGL extends EventEmitter {
         if (!bounds) {
             return;
         }
+
+        this._doExitDrawRegion();
 
         const skin = /** @type {PenSkin} */ this._allSkins[penSkinID];
 
