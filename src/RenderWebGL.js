b9732c222c060805101e2ccb79da282141240000
DD Liu <liudi08@gmail.com>
2019-08-28 19:00:16 -0400

    Fix space
    Co-Authored-By: adroitwhiz <adroitwhiz@protonmail.com>

--- src/RenderWebGL.js
+++ src/RenderWebGL.js
@@ -104,7 +104,7 @@ class RenderWebGL extends EventEmitter {
      * @private
      */
     static _getContext (canvas) {
-        return twgl.getWebGLContext(canvas, {alpha: false, stencil: true , antialias: false});
+        return twgl.getWebGLContext(canvas, {alpha: false, stencil: true, antialias: false});
     }
 
     /**
