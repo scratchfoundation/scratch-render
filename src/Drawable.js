ad6ddd9f9984d4dd8557a9c7959b1bb412c6da9e
adroitwhiz <adroitwhiz@protonmail.com>
2019-09-04 15:40:49 -0400

    add more parentheses

--- src/Drawable.js
+++ src/Drawable.js
@@ -434,12 +434,12 @@ class Drawable {
         }
 
         // If the effect bits for mosaic, pixelate, whirl, or fisheye are set, use linear
-        if (this._effectBits & (
+        if ((this._effectBits & (
             ShaderManager.EFFECT_INFO.fisheye.mask |
             ShaderManager.EFFECT_INFO.whirl.mask |
             ShaderManager.EFFECT_INFO.pixelate.mask |
             ShaderManager.EFFECT_INFO.mosaic.mask
-        ) !== 0) {
+        )) !== 0) {
             return false;
         }
 
