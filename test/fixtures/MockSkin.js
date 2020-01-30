d9c854f1fc41e89c94354e2706359c952cf65bb4
adroitwhiz <adroitwhiz@protonmail.com>
2020-01-30 01:04:33 -0500

    Remove setRotationCenter API

--- test/fixtures/MockSkin.js
+++ test/fixtures/MockSkin.js
@@ -8,6 +8,16 @@ class MockSkin extends Skin {
     get size () {
         return this.dimensions || [0, 0];
     }
+
+    set rotationCenter (center) {
+        this._rotationCenter[0] = center[0];
+        this._rotationCenter[1] = center[1];
+        this.emit(Skin.Events.WasAltered);
+    }
+
+    get rotationCenter () {
+        return this._rotationCenter;
+    }
 }
 
 module.exports = MockSkin;
