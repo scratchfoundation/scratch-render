0f6663f3148b4f994d58e19590e14c152f1cc2f8
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2021-10-26 14:20:36 -0700

    try WebGL 2.0 if WebGL 1.0 fails

--- src/RenderWebGL.js
+++ src/RenderWebGL.js
@@ -111,7 +111,12 @@ class RenderWebGL extends EventEmitter {
      * @private
      */
     static _getContext (canvas) {
-        return twgl.getWebGLContext(canvas, {alpha: false, stencil: true, antialias: false});
+        const contextAttribs = {alpha: false, stencil: true, antialias: false};
+        // getWebGLContext = try WebGL 1.0 only
+        // getContext = try WebGL 2.0 and if that doesn't work, try WebGL 1.0
+        // getWebGLContext || getContext = try WebGL 1.0 and if that doesn't work, try WebGL 2.0
+        return twgl.getWebGLContext(canvas, contextAttribs) ||
+            twgl.getContext(canvas, contextAttribs);
     }
 
     /**
