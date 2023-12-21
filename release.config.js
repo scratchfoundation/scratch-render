cb7098c8d4150d822281fee98c771f340a67f287
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2023-12-20 19:07:52 -0800

    ci: use semantic-release

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: 'scratch-semantic-release-config',
+    branches: [
+        {
+            name: 'develop'
+            // default channel
+        }
+        // TODO: add hotfix config after at least one normal release
+    ]
+};
